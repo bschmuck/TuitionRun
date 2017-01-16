7058847bb3840ad9d94f09ac44524535b9db3f09
Brandon Schmuck <bschmuck@BrandonMacBook4.wv.cc.cmu.edu>
2017-01-16 18:16:13 -0500

    Initial Commit.

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	bool didUpdate = true;
+	float secondsElapsed = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!didUpdate && secondsElapsed >= 3) {
+			transform.RotateAround (transform.position, transform.up, 180f);
+			didUpdate = true;
+		}
+		secondsElapsed += Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+	private Vector3 moveDirection = Vector3.zero;
+	public float gravity = 20f;
+	private CharacterController controller;
+	private Animator anim;
+
+	public float JumpSpeed = 8.0f;
+	public float Speed = 6.0f;
+
+	// Use this for initialization
+	void Start () {
+		moveDirection = transform.forward;
+		moveDirection = transform.TransformDirection (moveDirection);
+		moveDirection *= Speed;
+		controller = GetComponent<CharacterController> ();
+		anim = GetComponentInChildren<Animator> ();
+		anim.SetBool ("started", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate(-Vector3.right * Speed * Time.deltaTime);
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate(Vector3.right * Speed * Time.deltaTime);
+		}
+
+		controller.Move (moveDirection * Time.deltaTime);
+	}
+}
