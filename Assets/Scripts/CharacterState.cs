cd96b671eef728fcdb1e97c4ee31bd363f159162
Brandon Schmuck <bschmuck@BrandonMacBook4.wv.cc.cmu.edu>
2017-01-17 21:07:35 -0500

    Fixed bad gravity issue.

---
+++ Assets/Scripts/CharacterState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterState : MonoBehaviour {
+
+	public bool isGrounded = true;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		}
+		else {
+			DestroyImmediate(this);
+		}
+	}
+
+	//singleton implementation
+	private static CharacterState instance;
+	public static CharacterState Instance {
+		get {
+			if (instance == null) {
+				instance = new CharacterState ();
+			}
+			return instance;
+		}
+	}
+}
---
+++ Assets/Scripts/DetectGround.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DetectGround : MonoBehaviour {
+
+	void OnTriggerEnter() {
+		print ("Grounded!");
+		CharacterState.Instance.isGrounded = true;
+	}
+
+	void OnTriggerStay() {
+		CharacterState.Instance.isGrounded = true;
+	}
+
+	void OnTriggerExit() {
+		print ("Not Grounded!");
+		CharacterState.Instance.isGrounded = false;
+	}
+}
